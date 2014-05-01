c558a2e59a5798f34000a7e74e18a371ba53ed92
= <=>
2014-05-01 16:23:25 +0200

    fix and added comments

--- src/Boyhagemann/Hydra/Utility/Form.php
+++ src/Boyhagemann/Hydra/Utility/Form.php
@@ -2,30 +2,66 @@
 
 namespace Boyhagemann\Hydra\Utility;
 
+use Boyhagemann\Hydra\HydraClass;
+
+/**
+ * Class Form
+ *
+ * @package Boyhagemann\Hydra\Utility
+ */
 class Form
 {
+	/**
+	 * @var Array
+	 */
 	protected $mapping;
 
+	/**
+	 * @var Array
+	 */
 	protected $elements;
 
+	/**
+	 * @var mixed
+	 */
 	protected $build;
 
+	/**
+	 * Map a property type to an element
+	 *
+	 * @param string $iri
+	 * @param $element
+	 */
 	public function map($iri, $element)
 	{
 		$this->mapping[$iri] = $element;
 	}
 
+	/**
+	 * Build the form element html
+	 *
+	 * @param string $name
+	 * @param $callback
+	 */
 	public function element($name, $callback)
 	{
 		$this->elements[$name] = $callback;
 	}
 
+	/**
+	 * Build the form html. The callback contains
+	 * an array with form element html.
+	 *
+	 * @param $callback
+	 */
 	public function build($callback)
 	{
 		$this->build = $callback;
 	}
 
 	/**
+	 * Render a form based on a Hydra object.
+	 *
 	 * @param HydraClass $hydra
 	 * @return string
 	 */
